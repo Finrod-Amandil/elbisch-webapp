8751042e4c5ed1dd6b0d4670074004f58223303d
Neria13 <silver.eagle@hotmail.de>
2018-10-03 10:51:10 +0200

    moved common resources to shared file

---
+++ views/common/resources-common.php
@@ -0,0 +1,21 @@
+<?php
+/*
+file: resources-common.php
+author: Severin Zahler, Nadine Seiler
+history:
+- 2018-10-03: Nadine Seiler: added file
+
+summary:
+HTML dependencies shared across all views.
+*/
+?>
+<link rel="icon" type="image/png" href="static/img/favicon.ico">
+
+<link rel="stylesheet" type="text/css" href="static/css/bootstrap.min.css">        <!-- bootstrap 4.0.0-alpha.6 -->
+<link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Roboto" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="static/css/styles-template.css">	   <!-- Stylesheet for template -->
+
+<script src="https://use.fontawesome.com/531bdc0c02.js"></script>			       <!-- FontAwesome for Glyphicons -->
+<script type="text/javascript" src="static/js/jquery-3.2.1.min.js"></script>       <!-- jQuery 3.2.1 -->
+<script type="text/javascript" src="static/js/tether.min.js"></script>             <!-- tether -->
+<script type="text/javascript" src="static/js/bootstrap.min.js"></script>          <!-- bootstrap 4.0.0-alpha.6 -->
