28f33e7bef4429019fa0f32140e7e4b791d88d79
Severin Zahler <severin.zahler@noser.com>
2018-10-03 09:07:23 +0200

    Basic MVC structure from tutorial
    Source: https://www.youtube.com/watch?v=DpbUqJcch0Y

---
+++ Routes.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::set('index.php', function() {
+	AboutUs::CreateView('Index');
+});
+
+Route::set('about-us', function() {
+	AboutUs::CreateView('AboutUs');
+});
+
+Route::set('contact-us', function() {
+	ContactUs::CreateView('ContactUs');
+});
+
+?>
