32acc7bd5692a979fd70e38829ba054a0b479066
Severin Zahler <severin.zahler@noser.com>
2018-10-28 08:21:35 +0100

    Target views for login / register feedback

---
+++ views/Register.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		
+		<?php $page = "register"; ?>
+		<?php include 'views/common/resources-common.php'; ?>
+		<link rel="stylesheet" type="text/css" href="static/css/styles-index.css">
+		
+		<title>elbisch.ch - Registrieren</title>
+		
+	</head>
+
+	<body>
+		<div id="viewport" class="container-fluid">
+		
+			<!-- Navigation -->
+			<?php include 'views/common/navbar.php'; ?>
+		
+			<!-- Background -->
+			<div id="bg-left" class="bg-left"></div>
+			<div id="bg-right" class="bg-right"></div>
+			
+			<div class="row justify-content-center">
+				<div id="container-outer" class="container-outer col-sm-12 col-md-12 col-lg-8">
+					<div class="container-inner">
+						<h1>Registrieren</h1>
+						<p><?php if (isset($registerMessage)) { echo ($registerMessage); } ?></p>
+					</div>
+				</div>
+			</div>
+			
+			<!-- Footer -->
+			<?php include 'views/common/footer.php'; ?>
+		</div>
+
+		
+	</body>
+
+</html>
