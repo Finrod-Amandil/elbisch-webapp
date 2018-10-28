4a23aab17299a4a0374b056aca9b1d3a44f0e047
severin.zahler <severin.zahler@noser.com>
2018-10-28 19:28:44 +0100

    Backend validation and saving to database of new order

--- controllers/OrderFormController.php
+++ controllers/OrderFormController.php
@@ -9,8 +9,92 @@ summary:
 Controller for OrderForm view.
 */
 
-class OrderFormController extends Controller {}
+require_once("./helpers/OrderFormConstants.php");
+require_once("./models/Order.php");
+require_once("./persistence/HpDataDbContext.php");
+
+class OrderFormController extends Controller {
 	public static function Submit() {
 		
+		$name = isset($_POST["name"]) ? $_POST["name"] : '';
+		
+		if (!isset($_POST["email"]) or 
+		    !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+			$orderSubmitMessage = "Anfrage konnte nicht gespeichert werden: Ungültige E-mail Adresse";
+			require_once("./views/OrderSubmitted.php");
+			return;
+		}
+		else {
+			$email = $_POST["email"];
+		}
+		
+		if (!isset($_POST["order_type"]) or $_POST["order_type"] == "0") {
+			$orderSubmitMessage = "Anfrage konnte nicht gespeichert werden: Ungültiger Anfragetyp";
+			require_once("./views/OrderSubmitted.php");
+			return;
+		}
+		$orderType = $_POST["order_type"];
+		
+		$languages = '';
+		foreach (OrderFormConstants::LANGUAGE_NAMES as $language) {
+			if (isset($_POST["language_" . $language])) {
+				$languages = $languages . $language . ",";
+			}
+		}
+		
+		$fonts = '';
+		foreach (OrderFormConstants::FONT_NAMES as $font) {
+			if (isset($_POST["script_" . $font])) {
+				$fonts = $fonts . $font . ",";
+			}
+		}
+		
+		if (!isset($_POST["order-description"]) or $_POST["order-description"] == '') {
+			$orderSubmitMessage = "Anfrage konnte nicht gespeichert werden: Auftragsbeschreibung fehlt.";
+			require_once("./views/OrderSubmitted.php");
+			return;
+		}
+		$orderDescription = $_POST["order-description"];
+		
+		$derivation = isset($_POST["extra_derivation"]);
+		$offer = isset($_POST["extra_offer"]);
+		$gallery = isset($_POST["gallery"]);
+		
+		if (!isset($_POST["payment"])) {
+			$orderSubmitMessage = "Anfrage konnte nicht gespeichert werden: Bezahlungsart fehlt.";
+			require_once("./views/OrderSubmitted.php");
+			return;
+		}
+		$payment = $_POST["payment"];
+		
+		if (!isset($_POST["currency"])) {
+			$orderSubmitMessage = "Anfrage konnte nicht gespeichert werden: Rechnungswährung fehlt.";
+			require_once("./views/OrderSubmitted.php");
+			return;
+		}
+		$currency = $_POST["currency"];
+		
+		$comments = isset($_POST["comments"]) ? $_POST["comments"] : '';
+		
+		$order = new Order();
+		$order->name = $name;
+		$order->email = $email;
+		$order->orderType = $orderType;
+		$order->languages = $languages;
+		$order->fonts = $fonts;
+		$order->orderDescription = $orderDescription;
+		$order->derivation = $derivation;
+		$order->offer = $offer;
+		$order->gallery = $gallery;
+		$order->payment = $payment;
+		$order->currency = $currency;
+		$order->comments = $comments;
+		
+		$dbContext = new HpDataDbContext();
+		$dbContext->addOrder($order);
+		
+		$orderSubmitMessage = "Auftrag erfolgreich gespeichert!";
+		require_once("./views/OrderSubmitted.php");
 	}
+}
 ?>
--- helpers/OrderFormConstants.php
+++ helpers/OrderFormConstants.php
@@ -1,21 +1,25 @@
 <?php
 
-class OrderFormConstants
+class OrderFormConstants {
 
-public static LANGUAGE_NAMES = array("quenya", "sindarin");
+	public const ORDER_TYPE_NAMES = array("translation-transcription", "transcription", "translation", "other");
 
-public static SCRIPT_NAMES = 
-	array("tengwar-annatar", "tengwar-annatar-bold", "tengwar-annatar-italic", "tengwar-annatar-bold-italic", 
-	      "tengwar-noldor", "tengwar-noldor-caps", "tengwar-quenya", "tengwar-quenya-caps", "tengwar-sindarin",
-		  "tengwar-sindarin-caps", "tengwar-beleriand", "tengwar-elfica", "tengwar-formal", "tengwar-parmaite",
-		  "tengwar-eldamar", "tengwar-galvorn", "tengwar-mornedhel", "tengwar-telerin", "tengwar-hereno",
-		  "elfic-caslon", "tengwar-gothika", "greifswalder-tengwar", "tengwar-optime", "cirth-erebor",
-		  "cirth-erebor-caps", "cirth-erebor-1", "cirth-erebor-2");
-		  
-public static EXTRA_NAMES = array("derivation", "offer");
+	public const LANGUAGE_NAMES = array("quenya", "sindarin");
 
-public static PAYMENT_NAMES = array("e-banking", "paypal");
+	public const FONT_NAMES = 
+		array("tengwar-annatar", "tengwar-annatar-bold", "tengwar-annatar-italic", "tengwar-annatar-bold-italic", 
+			  "tengwar-noldor", "tengwar-noldor-caps", "tengwar-quenya", "tengwar-quenya-caps", "tengwar-sindarin",
+			  "tengwar-sindarin-caps", "tengwar-beleriand", "tengwar-elfica", "tengwar-formal", "tengwar-parmaite",
+			  "tengwar-eldamar", "tengwar-galvorn", "tengwar-mornedhel", "tengwar-telerin", "tengwar-hereno",
+			  "elfic-caslon", "tengwar-gothika", "greifswalder-tengwar", "tengwar-optime", "cirth-erebor",
+			  "cirth-erebor-caps", "cirth-erebor-1", "cirth-erebor-2");
+			  
+	public const EXTRA_NAMES = array("derivation", "offer");
 
-public static CURRENCY_NAMES = array("chf", "eur");
+	public const PAYMENT_NAMES = array("e-banking", "paypal");
+
+	public const CURRENCY_NAMES = array("chf", "eur");
+
+}
 
 ?>
